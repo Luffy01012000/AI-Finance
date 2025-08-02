d6fd0e005f262a6df8b5b16786211b10c3dcca22
Goku <goku05555@gmail.com>
2025-08-02 20:04:50 +0530

    refactor: change resend mail to gmail

--- actions/send-email.js
+++ actions/send-email.js
@@ -1,18 +1,43 @@
 "use server";
 
-import { Resend } from "resend";
+// import { Resend } from "resend";
+
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.SMTP_MAIL,
+    pass: process.env.SMTP_PASSWORD,
+  },
+});
 
 export async function sendEmail({ to, subject, react }) {
-  const resend = new Resend(process.env.RESEND_API_KEY || "");
 
   try {
-    const data = await resend.emails.send({
-      from: "Finance App <onboarding@resend.dev>",
-      to,
-      subject,
-      react,
-    });
+    // const data = await resend.emails.send({
+    //   from: "Finance App <onboarding@resend.dev>",
+    //   to,
+    //   subject,
+    //   react,
+    // });
 
+  const data = await  transporter.sendMail(
+  {
+    from: "Finance App <onboarding@resend.dev>",
+    to,
+    subject,
+    text: react,
+  },
+  // (err, info) => {
+  //   if (err) {
+  //     console.error(err);
+  //     return;
+  //   }
+  //   console.log(info.envelope);
+  //   console.log(info.messageId);
+  // }
+);
     return { success: true, data };
   } catch (error) {
     console.error("Failed to send email:", error);
